5d232a8a61e98809fa16bda268d8b4a3a93773f4
[dyad] <heisenbergue342@gmail.com>
2025-10-18 20:10:11 +0200

    [dyad] wrote 3 file(s)

---
+++ src/pages/Altenheime.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+const Altenheime = () => {
+  return (
+    <div>
+      <h1>Altenheime</h1>
+      {/* Hier können Sie die Liste der Altenheime einfügen */}
+    </div>
+  );
+};
+
+export default Altenheime;
---
+++ src/pages/Kliniken.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+
+const Kliniken = () => {
+  return (
+    <div>
+      <h1>Kliniken</h1>
+      {/* Hier können Sie die Liste der Kliniken einfügen */}
+    </div>
+  );
+};
+
+export default Kliniken;
